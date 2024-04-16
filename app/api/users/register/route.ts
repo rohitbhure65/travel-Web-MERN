3fb00d02e2b2913b93013731ee81641da74b0d54
Rohit Bhure <rohitbhure.cse@gmail.com>
2024-04-16 15:35:51 +0530

    all bug fixed

--- app/api/users/register/route.ts
+++ app/api/users/register/route.ts
@@ -29,7 +29,8 @@ export async function POST(request: NextRequest){
                 const newUser = new User({name,email,phone,password})
             
                 const savedUser = await newUser.save()
-                // console.log(savedUser)   
+                console.log(savedUser)   
+
 
                 return NextResponse.json({ 
                     message: "User Created Successfully",
